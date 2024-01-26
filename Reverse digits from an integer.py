1c1aa1116cd5d8830c61b16c019d679898720811
Gerald <geraldonod@gmail.com>
2024-01-26 12:04:31 +0800

    loop that reverses the digits

--- Reverse digits from an integer.py
+++ Reverse digits from an integer.py
@@ -3,4 +3,7 @@ number = int(input("Enter a number: "))
 print("Given number", number)
 
 #loop that reverses the digits
-
+while number > 0:
+    digit = number % 10
+    number = number // 10
+    print(digit, end=" ")
